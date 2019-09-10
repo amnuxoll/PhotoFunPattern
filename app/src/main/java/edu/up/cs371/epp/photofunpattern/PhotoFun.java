83e30b3820f5ae66d725587703a2bddbe344f22e
amnuxoll <amnuxoll@hotmail.com>
2019-09-10 13:12:19 -0700

    A useless example comment for demo

--- app/src/main/java/edu/up/cs371/epp/photofunpattern/PhotoFun.java
+++ app/src/main/java/edu/up/cs371/epp/photofunpattern/PhotoFun.java
@@ -1,5 +1,6 @@
 package edu.up.cs371.epp.photofunpattern;
 
+//a useless comment
 import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
         import android.graphics.Bitmap;
